84dd5fed073dc0cb836745d12899b903d90b341c
yoshioka0101 <yuuki0205nano@gmail.com>
2025-09-14 14:49:31 +0900

    zap で log を出すように設定

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,10 +4,19 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yoshioka0101/ai_plane_chat/internal/middleware"
 )
 
 func main() {
-	r := gin.Default()
+	logger := middleware.NewLogger()
+	defer logger.Sync()
+
+	r := gin.New()
+
+	// middleware として logger を設定
+	r.Use(middleware.Logger(logger))
+	// panic してもここで回復する｀
+	r.Use(gin.Recovery())
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
